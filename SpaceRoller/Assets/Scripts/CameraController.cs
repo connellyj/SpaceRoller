b6ef14d7ee7656b21ddb5636b61993e9f6e82b8c
Julia Connelly <juliakconnelly@gmail.com>
2016-10-17 19:14:56 +0200

    added player and camera controller to the sandbox

---
+++ SpaceRoller/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+
+    Vector3 offset;
+
+    void Start() {
+        offset = getOffset();
+    }
+
+    void LateUpdate() {
+        transform.position = player.transform.position + offset;
+        float h = 0f;
+        if(Input.GetKey(KeyCode.X)) h = -speed * Time.deltaTime;
+        if(Input.GetKey(KeyCode.Z)) h = speed * Time.deltaTime;
+        if(h != 0f) {
+            transform.RotateAround(player.transform.position, Vector3.up, h);
+            offset = getOffset();
+        }
+    }
+
+    Vector3 getOffset() {
+        return transform.position - player.transform.position;
+    }
+}
---
+++ SpaceRoller/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        Vector3 cameraRight = Camera.main.transform.right;
+        Vector3 cameraForward = Quaternion.AngleAxis(Camera.main.transform.rotation.x * -180, cameraRight) * Camera.main.transform.forward;
+
+        Vector3 moveHorizontal = Input.GetAxis("Horizontal") * cameraRight;
+        Vector3 moveVertical = cameraForward * Input.GetAxis("Vertical");
+
+        Vector3 movement = moveVertical + moveHorizontal;
+
+        rb.AddForce(movement * speed);
+    }
+}
