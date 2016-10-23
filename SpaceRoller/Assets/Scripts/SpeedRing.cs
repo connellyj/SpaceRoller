3df5efa4a090a2484f2df72b2f00b216d6aa41f3
Connor Levesque <connorlevesque5@gmail.com>
2016-10-23 14:48:33 +0200

    Fixed speedrings

--- SpaceRoller/Assets/Scripts/SpeedRing.cs
+++ SpaceRoller/Assets/Scripts/SpeedRing.cs
@@ -4,17 +4,26 @@ using System.Collections;
 public class SpeedRing : MonoBehaviour {
 
 	public float strength;
-	public Vector3 direction;
+	public Vector3 normal;
 
 	void Start()
 	{
-		direction = direction.normalized;
+		MeshFilter filter = GetComponent<MeshFilter>();
+  		normal = filter.transform.TransformDirection(filter.mesh.normals[40]); 
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
+			Vector3 distance = transform.position - other.gameObject.transform.position;
+			Vector3 direction = normal;
+			if (Vector3.Dot(distance, normal) < 0)
+			{
+				direction *= -1;
+			}
+
+
 			Rigidbody rb = other.GetComponent<Rigidbody>();
             rb.AddForce(direction * strength);
 		}
